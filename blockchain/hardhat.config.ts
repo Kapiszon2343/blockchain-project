960293708ebaa3725f2d324239a4e2b8e2d490bd
Kapiszon2343 <harasimowiczk@wp.pl>
2025-06-28 00:07:16 +0200

    sepolia contract ignition

--- blockchain/hardhat.config.ts
+++ blockchain/hardhat.config.ts
@@ -1,5 +1,8 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+
+dotenv.config({ path: __dirname + "/../.env" });
 
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
@@ -8,8 +11,15 @@ const config: HardhatUserConfig = {
       accounts: {
         mnemonic: "test test test test test test test test test test test junk",
       }
-    }
-  }
+    },
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL || "",
+      accounts: process.env.PRIVATE_KEY ? [process.env.PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
